4a1aa335cbbfed9457d82bc79f810ea875a076fb
SadeEtensh <eteneshsade550305@gmail.com>
2023-11-04 16:26:44 +0300

    addtional reducers with rtk

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,6 +1,6 @@
 import type { RootState } from "../../app/store";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, incrementByAmount } from "./counterSlice";
 
 export default function Counter() {
   const count = useSelector((state: RootState) => state.counter.value);
@@ -21,6 +21,12 @@ export default function Counter() {
           onClick={() => dispatch(decrement())}
         >
           Decrement
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(incrementByAmount(10))}
+        >
+          IncrementByAmount
         </button>
       </div>
     </div>
